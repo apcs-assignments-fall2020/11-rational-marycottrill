69fb2d071957aacf02ef7845b9453431764f61ce
marycottrill <21mcottrill@athenian.org>
2020-10-23 05:30:43 +0000

    Rational Code

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -26,6 +26,7 @@ public class Rational
         int newDenominator = r.denominator*s.denominator; 
         
         Rational x = new Rational(newNumerator, newDenominator);
+        x = simplify(x);
         return x;
     }
 
@@ -50,28 +51,44 @@ public class Rational
     //      simplify(1/2) => 1/2
     public static Rational simplify(Rational r)
     {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int factor = greatestCommonFactor(r.numerator, r.denominator);
+        int simplifiednum = r.numerator / factor; 
+        int simplifieddenom = r.denominator / factor;
+        Rational n = new Rational (simplifiednum, simplifieddenom);
+
+        return n;
     }
 
     // This method takes two Rationals, subtracts thems up, 
     // and returns a Rational equal to the difference
     public static Rational subtract(Rational r, Rational s)
     {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int newNumerator = r.numerator*s.denominator - s.numerator*r.denominator;
+        int newDenominator = r.denominator*s.denominator; 
+        
+        Rational x = new Rational(newNumerator, newDenominator);
+        x = simplify(x);
+        return x; 
     }
     
     public static Rational multiply(Rational r, Rational s)
     {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int newNumerator = r.numerator * s.numerator;
+        int newDenominator = r.denominator * s.denominator; 
+
+        Rational x = new Rational(newNumerator, newDenominator);
+        x = simplify(x);
+        return x;
     }
     
     public static Rational divide(Rational r, Rational s)
     {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int newNumerator = r.numerator*s.denominator;
+        int newDenominator = r.denominator*s.numerator; 
+        
+        Rational x = new Rational(newNumerator, newDenominator);
+        x = simplify(x);
+        return x; 
     }
 
 
